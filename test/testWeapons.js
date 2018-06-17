136ad066d0cb291e3ddb663d1c7d13c476dc41af
lalugeo <lalu.george@ifb-group.com>
2018-06-17 12:34:41 +0200

    testcase finalisations for extended games

--- test/testWeapons.js
+++ test/testWeapons.js
@@ -1,100 +1,24 @@
 /* eslint-env mocha */
 
 const { expect } = require('chai');
-const Game = require('../lib/control/game.js');
+const Weapon = require('../lib/control/weapon.js');
 
-let newGame;
-const rock = {
-  name: 'rock',
-  image: '/a/b/img1.png',
-  rank: 0,
-};
-const paper = {
-  name: 'paper',
-  image: '/a/b/img2.png',
-  rank: 1,
-};
-const scissors = {
-  name: 'scissors',
-  image: '/a/b/img3.png',
-  rank: 2,
-};
+let someWeapon;
+
 describe('weapon class test', () => {
   before(() => {
-    newGame = new Game([rock, paper, scissors]);
+    someWeapon = new Weapon('rock', '/a/b/img.png', 1);
   });
 
-  it('loading of weapons should happen correctly', () => {
-    expect(newGame.getWeapons())
-      .to.deep.equal([
-        rock,
-        paper,
-        scissors,
-      ]);
+  it('get name should return correct', () => {
+    expect(someWeapon.getName()).to.equal('rock');
   });
 
-  it('fetching scissors should return scissors', () =>
-    newGame.getWeaponNamedAs('scissors')
-      .then((weapon) => {
-        expect(weapon)
-          .to.deep.equal(scissors);
-      }));
+  it('get image should return correct', () => {
+    expect(someWeapon.getImage()).to.equal('/a/b/img.png');
+  });
 
-  it('fetching non existant weapon should return error', () =>
-    newGame.getWeaponNamedAs('missile')
-      .catch(err => new Promise((resolve) => {
-        resolve(err);
-      }))
-      .then((raisedErr) => {
-        expect(raisedErr.message)
-          .to.deep.equal('weapon not found!');
-      }));
-
-  it('rock should blunt scissors', () =>
-    newGame.checkWhoWins('rock', 'scissors')
-      .then((winner) => {
-        expect(winner)
-          .to.deep.equal(rock);
-      }));
-
-  it('scissors should cut paper', () =>
-    newGame.checkWhoWins('paper', 'scissors')
-      .then((winner) => {
-        expect(winner)
-          .to.deep.equal(scissors);
-      }));
-
-  it('paper should cover rock', () =>
-    newGame.checkWhoWins('rock', 'paper')
-      .then((winner) => {
-        expect(winner)
-          .to.deep.equal(paper);
-      }));
-
-  it('scissors vs scissors should be a tie', () =>
-    newGame.checkWhoWins('scissors', 'scissors')
-      .then((winner) => {
-        expect(winner)
-          .to.deep.equal(false);
-      }));
-
-  it('even one non existant weapon in match should throw error', () =>
-    newGame.checkWhoWins('scissors', 'missile')
-      .catch(err => new Promise((resolve) => {
-        resolve(err);
-      }))
-      .then((raisedErr) => {
-        expect(raisedErr.message)
-          .to.deep.equal('weapon not found!');
-      }));
-
-  it('both non existant weapons in match should throw error', () =>
-    newGame.checkWhoWins('shark', 'missile')
-      .catch(err => new Promise((resolve) => {
-        resolve(err);
-      }))
-      .then((raisedErr) => {
-        expect(raisedErr.message)
-          .to.deep.equal('weapon not found!');
-      }));
+  it('get rank should return correct', () => {
+    expect(someWeapon.getRank()).to.equal(1);
+  });
 });
