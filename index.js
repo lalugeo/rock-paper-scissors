a35fe4ef329bc618c77fdacc254974a9dabcf638
lalugeo <lalu.george@ifb-group.com>
2018-06-17 11:24:39 +0200

    game and weapon test cases done

---
+++ index.js
@@ -0,0 +1,24 @@
+const Game = require('./lib/control/game.js');
+
+let newGame;
+const rock = {
+  name: 'rock',
+  image: '/a/b/img1.png',
+  rank: 0,
+};
+const paper = {
+  name: 'paper',
+  image: '/a/b/img2.png',
+  rank: 1,
+};
+const scissors = {
+  name: 'scissors',
+  image: '/a/b/img3.png',
+  rank: 2,
+};
+
+newGame = new Game([rock, paper, scissors]);
+newGame.checkWhoWins('rock', 'scissors')
+  .then((won) => {
+    console.log(JSON.stringify(won));
+  });
