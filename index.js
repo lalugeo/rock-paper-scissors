49ec9869ef7c1afcf4cc9da74ce804370a704937
lalugeo <lalu.george@ifb-group.com>
2018-06-17 19:00:33 +0200

    backennd linking with ui

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const http = require('http');
-const routes = require('./lib/view/routes.js');
+const routes = require('./lib/view/js/routes.js');
 
 const server = http.createServer((req, resp) => {
   if (req.url === '/' || req.url === '/newGame') {
     routes('newGame')(req, resp);
   } else if (req.url === '/match') {
     routes('match')(req, resp);
-  } else if (req.url.match(/^\/css/)) {
-    routes('css')(req, resp);
+  } else if (req.url.match(/\/css\//) || req.url.match(/\/ext_lib\//) || req.url.match(/\/view\/js\//)) {
+    routes('activeFile')(req, resp);
+  } else if (req.url.match(/\/svc\/initialise/)) {
+    routes('webServices').initialise(req, resp);
   } else {
     resp.writeHead(404, { 'Content-Type': 'text/html' });
     resp.write('No such page found!');
