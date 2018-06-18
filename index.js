9556b3961f69bdb1f7cfc031c551319b164d06eb
lalugeo <lalu.george@ifb-group.com>
2018-06-18 21:27:33 +0200

    more ui

--- index.js
+++ index.js
@@ -2,18 +2,28 @@ const http = require('http');
 const routes = require('./lib/view/js/routes.js');
 
 const server = http.createServer((req, resp) => {
-  if (req.url === '/' || req.url === '/newGame') {
-    routes('newGame')(req, resp);
-  } else if (req.url === '/match') {
-    routes('match')(req, resp);
-  } else if (req.url.match(/\/css\//) || req.url.match(/\/ext_lib\//) || req.url.match(/\/view\/js\//)) {
-    routes('activeFile')(req, resp);
-  } else if (req.url.match(/\/svc\/initialise/)) {
-    routes('webServices').initialise(req, resp);
-  } else {
-    resp.writeHead(404, { 'Content-Type': 'text/html' });
-    resp.write('No such page found!');
-    resp.end();
-  }
+  req.body = [];
+  req.on('data', (chunk) => {
+    req.body.push(chunk);
+  }).on('end', () => {
+    req.body = Buffer.concat(req.body).toString();
+    if (req.url === '/') {
+      routes('index')(req, resp);
+    } else if (req.url === '/newGame') {
+      routes('newGame')(req, resp);
+    } else if (req.url === '/match') {
+      routes('match')(req, resp);
+    }  else if (req.url.match(/\/css\//) || req.url.match(/\/ext_lib\//) || req.url.match(/\/view\/js\//)) {
+      routes('activeFile')(req, resp);
+    } else if (req.url.match(/\/svc\/initialise/)) {
+      routes('webServices').initialise(req, resp);
+    } else if (req.url.match(/\/svc\/fetchArmoury/)) {
+      routes('webServices').fetchArmoury(req, resp);
+    } else {
+      resp.writeHead(404, { 'Content-Type': 'text/html' });
+      resp.write('No such page found!');
+      resp.end();
+    }
+  });
 });
 server.listen(5050);
